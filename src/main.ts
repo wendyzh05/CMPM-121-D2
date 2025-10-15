8caccaf39477d1881be2b6a46d944f33971ad8e9
Wendy Zhang <156264212+wendyzh05@users.noreply.github.com>
2025-10-15 17:15:59 +0000

    Added clear button and marker to draw (Step 2 complete)
    Co-authored-by: t4ylo <t4ylo@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "./style.css";
+type Cursor = { active: boolean; x: number; y: number };
+const cursor: Cursor = { active: false, x: 0, y: 0 };
 
 function createAppTitle(titleText: string): HTMLElement {
   const title = document.createElement("h1");
@@ -20,10 +22,44 @@ function initUI(): void {
   document.body.appendChild(app);
 
   const title = createAppTitle("Sticker Sketchpad");
-  const canvas = createDrawingCanvas(256, 256);
+  const canvas = createDrawingCanvas(450, 450);
+  const ctx = canvas.getContext("2d");
+  if (!ctx) throw new Error("Failed to get 2D context");
+
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
 
   app.appendChild(title);
   app.appendChild(canvas);
-}
 
+  canvas.addEventListener("mousedown", (e: MouseEvent) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+  });
+
+  canvas.addEventListener("mousemove", (e: MouseEvent) => {
+    if (!cursor.active) return;
+
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+
+  const clearButton = document.createElement("button");
+  clearButton.innerHTML = "clear";
+  document.body.append(clearButton);
+
+  clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+}
 initUI();
